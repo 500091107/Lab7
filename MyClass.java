c64d0bed4db2f6c0e3344cc322cc4b39d046b941
Sanskar Sisodia <500091107@stu.upes.ac.in>
2022-09-27 14:54:12 +0530

    Second commit

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,7 @@
 package pkg1;
 
+import java.net.SocketTimeoutException;
+
 public class MyClass {
     int a, b;
 
@@ -10,6 +12,13 @@ public class MyClass {
         int c = a * b ;
        System.out.println ( " Multiplication is : " + c ) ;
    }
+
+    public static void sum(int x, int y) {
+        a = x;
+        b = y;
+        int c = a * b;
+        System.out.println("sum is" + c);
+    }
 
    public static void main ( String [ ] args ) {
        MyClass ob = new MyClass ( ); I
